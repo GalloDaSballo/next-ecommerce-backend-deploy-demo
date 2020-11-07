37779ec177850887e1f39259611beec62777f92a
Entreprenerd <valeranijr@gmail.com>
2020-11-07 22:43:12 +0100

    Initial Commit with products

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e7feb026c605850fc751a780ef63ed7b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1d5dbd4e-7892-4e52-a4a4-999630cc38b7'
+};
