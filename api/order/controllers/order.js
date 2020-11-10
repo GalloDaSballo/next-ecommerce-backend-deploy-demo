587732c171237276c5ecea446116372466232fd2
Entreprenerd <valeranijr@gmail.com>
2020-11-10 12:33:28 +0100

    fix remove useless import

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,6 +1,5 @@
 'use strict';
 const { sanitizeEntity } = require('strapi-utils');
-const finder = require('strapi-utils/lib/finder');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
