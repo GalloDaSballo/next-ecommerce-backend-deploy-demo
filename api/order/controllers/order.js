e808df50f81edac0c45d98e10a6a43d64b91057d
Entreprenerd <valeranijr@gmail.com>
2020-11-10 12:32:04 +0100

    34 Find and Find One Overridden

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,31 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
+const finder = require('strapi-utils/lib/finder');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async find(ctx) {
+        const { user } = ctx.state //This is the magic user
+
+        let entities
+        if(ctx.query._q) {
+            entities = await strapi.services.order.search({...ctx.query, user: user.id})
+        } else {
+            entities = await strapi.services.order.find({...ctx.query, user: user.id})
+        }
+
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.order }))
+    },
+
+    async findOne(ctx) {
+        const { id } = ctx.params
+        const { user } = ctx.state
+
+        const entity = await strapi.services.order.findOne({ id, user: user.id })
+        return sanitizeEntity(entity, { model: strapi.models.order })
+    }
+};
